833ccd0cb851173f2eda5c0b11d5f8442a5e6507
Brayahn <brayahnmainah@gmail.com>
2024-01-05 06:37:02 +0300

    BBooleans Numbers

--- 2 Data Types/script.js
+++ 2 Data Types/script.js
@@ -6,5 +6,44 @@
 
 This is a multiline Comment 
 
+can use cmd + / as a shortcut for multiline comments 
 
 */
+
+// simple strings
+const singleQuote = "Hello";
+const doubleQuotes = "Hello";
+
+//complex strings
+const persona = "Brian";
+const backticks = `Hello, ${persona}`;
+
+console.log(backticks);
+
+//type of datatype
+console.log(typeof backticks);
+
+/*
+NUMBERS
+*/
+const num1 = 2;
+let num3 = 2;
+
+const result = num1 + num3;
+console.log(result);
+
+/*
+
+Booleans
+
+*/
+const isCool = true;
+
+if (isCool) {
+  console.log("You are cool");
+} else {
+  console.log("You are uncool...");
+}
+
+//true  - yes, correct, 1
+//false - no, incorrect, 0
