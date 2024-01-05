b5ef9e005ac49b5ca0420f1504c726d3a2fd9035
Brayahn <brayahnmainah@gmail.com>
2024-01-05 05:47:44 +0300

    variables defined

--- Variables/script.js
+++ Variables/script.js
@@ -1,3 +1,18 @@
 var variableName = "welcome to Variables";
 
-console.log(variableName);
+//
+//
+//the preffered way
+let variablename2 = "";
+
+//re-assigning
+variablename2 = "value x";
+
+console.log(variablename2);
+
+//
+//
+//cannot be changed throught the program
+const variable3 = "Variable Constant";
+
+console.log(variableName, variable3);
