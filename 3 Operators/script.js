37833eebd59f8485a58002d743e46e6eccc067cb
Brayahn <brayahnmainah@gmail.com>
2024-01-09 05:45:40 +0300

    Comparison operators

--- 3 Operators/script.js
+++ 3 Operators/script.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * ARITHMETIC OPERATORS
+ *
+ */
 const a = 5;
 const b = 10;
 
@@ -26,3 +31,31 @@ result++;
 result--;
 
 console.log(result);
+
+/**
+ *
+ * COMPARISON OPERATORS  >>> true/false
+ *
+ */
+
+const c = 10;
+const d = 20;
+
+//less than or equal 
+console.log(c <= d);
+
+//greater than
+console.log(d > c);
+
+//Equality operator
+console.log(d == c);
+
+//Not Equal 
+console.log(d !== c);
+
+
+
+//Strict Equality 
+console.log(a===b);
+//Strict Inequality
+console.log(a!===b);
