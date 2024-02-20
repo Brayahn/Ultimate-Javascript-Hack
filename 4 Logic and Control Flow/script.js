d6fed34dd3432e70e8dc7c0edeabf7f39df1a81a
Brayahn <brayahnmainah@gmail.com>
2024-02-20 04:30:09 +0300

    Logic and conttrol flow - if statements

---
+++ 4 Logic and Control Flow/script.js
@@ -0,0 +1,13 @@
+/*
+*
+IF STATEMENTS 
+*/
+let age = 12;
+
+if (age > 18) {
+  console.log("You may enter");
+} else if (age === 18) {
+  console.log("you just turned 18");
+} else {
+  console.log("You are underage!");
+}
