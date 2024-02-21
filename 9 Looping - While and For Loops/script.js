6a66b1c5489b5f011c1bc6f58bc911548dca4b15
Brayahn <brayahnmainah@gmail.com>
2024-02-21 05:19:40 +0300

    For and While Loops

---
+++ 9 Looping - While and For Loops/script.js
@@ -0,0 +1,24 @@
+/**
+ * While Statement
+ */
+//
+//
+let i = 0;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+
+
+/**
+ * 
+ * 
+ for ([initialization];[condition];[Final Expression])
+ {statement};
+ */
+
+for (let j = 0; j <= 3; j++) {
+  console.log(j);
+}
