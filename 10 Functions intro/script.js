ae23290a9cad829e3ad2892f19cafe159c892348
Brayahn <brayahnmainah@gmail.com>
2024-02-22 04:48:42 +0300

    Declaring and invoking Functions

--- 10 Functions intro/script.js
+++ 10 Functions intro/script.js
@@ -7,6 +7,6 @@ function square(number) {
   return number * number;
 }
 
-//A function call / Execution
+//A function call / Execution / Invoking 
 const result = square(5);
 console.log(result);
---
+++ 11 Declaring and invoking Functions/script.js
@@ -0,0 +1,26 @@
+/***
+ * Functions Intro
+ */
+
+//Function declaration
+function name(params) {
+  //statements
+  //have access to this keyword
+}
+
+//Function expression / Anonymous Function
+const name2 = function (params) {
+  //statement(s)
+};
+
+//Arrow Function - modern way and more precise
+const name3 = (params) => {
+  //statements
+};
+
+function sayHi(name) {
+  console.log(`Hi, ${name}!`);
+}
+
+
+sayHi("Brian");
