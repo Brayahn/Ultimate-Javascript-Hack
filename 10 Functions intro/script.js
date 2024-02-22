303ba2bf98ebdbaede595754ea30a17fd6436447
Brayahn <brayahnmainah@gmail.com>
2024-02-22 04:31:21 +0300

    Functions intro

---
+++ 10 Functions intro/script.js
@@ -0,0 +1,12 @@
+/***
+ * Functions Intro
+ */
+
+//Function declaration
+function square(number) {
+  return number * number;
+}
+
+//A function call / Execution
+const result = square(5);
+console.log(result);
