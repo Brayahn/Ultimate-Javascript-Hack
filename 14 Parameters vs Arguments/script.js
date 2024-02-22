646d6b6a189718170da85af7a4dc1520f18f7e2f
Brayahn <brayahnmainah@gmail.com>
2024-02-22 05:44:13 +0300

    Parameters vs Arguemnts

---
+++ 14 Parameters vs Arguments/script.js
@@ -0,0 +1,23 @@
+/**
+ Parameters vs Arguments 
+ */
+
+//Parameters - Used when definiing a fucntion
+
+//Argumnets - Passed when making a function call
+
+const square = (number) => {
+  return number * number;
+};
+
+let result = square(50);
+console.log(result);
+
+/**
+ *
+ 
+from the abbove funciton 
+(number)   >>> parameter
+(50)   >>> argument
+
+ */
