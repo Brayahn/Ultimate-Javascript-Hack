919bf5483bd9a2d88aec116952054d28650b5b4f
Brayahn <brayahnmainah@gmail.com>
2024-02-22 05:09:42 +0300

    Arrow Functions

---
+++ 13 Arrow Functions/script.js
@@ -0,0 +1,19 @@
+/**
+ Arrow Functions
+ */
+
+// do not have this reserved keyword
+//the parenthisis can be ignored if there's one parameter
+const square = (number) => {
+  return number * number;
+};
+
+let result = square(50);
+console.log(result);
+
+//Short Coincise version //if there's one return statement
+//the parenthisis can be ignored if there's one parameter
+const add = (number) => number + number;
+
+const result2 = add(4);
+console.log(result2);
