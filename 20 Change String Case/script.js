18d0b1369a398231a40a6abeb5116b2c7b7b6b5d
Brayahn <brayahnmainah@gmail.com>
2024-03-18 06:02:43 +0300

    Change string case

---
+++ 20 Change String Case/script.js
@@ -0,0 +1,18 @@
+const alpha = "This is a Mixed CASe StrinG";
+
+const alpha2 = alpha.toLowerCase();
+const alpha3 = alpha.toUpperCase();
+
+console.log(
+  alpha,
+  `
+`,
+  alpha2,
+  `
+`,
+  alpha3
+);
+
+/***
+ Notice how we have paranthesses on these two. That's because they are functions, more precisely methods we call on a string.
+ */
