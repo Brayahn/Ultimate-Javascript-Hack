c3da753676fd8ef08d9f4ec6a4915335d0763eb4
Brayahn <brayahnmainah@gmail.com>
2024-03-18 05:24:00 +0300

    Strings intro

---
+++ 18 Strings Intro/script.js
@@ -0,0 +1,29 @@
+/**
+ Simple Strings 
+ */
+const single = "Single quotes";
+const double = "Double quotes";
+
+/**
+ Complex strings
+ */
+const backtick = `This is not a ${single} or a ${double} quotes example`;
+
+console.log(backtick);
+
+/**
+ Escape characters in strings
+ */
+const third = 'People call me Brian, or "Brayo" ';
+console.log(third);
+
+/**
+ Rows in Strings, Simple strings don't support this. 
+ */
+
+const numbers = `1
+2
+3
+4`;
+
+console.log(numbers);
