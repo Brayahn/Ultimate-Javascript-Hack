6e37945cf17a1e5808440293f7503137bd5d14e7
Brayahn <brayahnmainah@gmail.com>
2024-03-18 05:37:29 +0300

    String length and basic properties

---
+++ 19 String Length and Basic Properties/script.js
@@ -0,0 +1,13 @@
+/**
+ String Length
+ */
+const name1 = "Brian";
+
+//length
+console.log(name1.length);
+
+//first character
+console.log(name1[0]);
+
+//Last character
+console.log(name1[name1.length - 1]);
