32f9493e985bc13bc81fdc01c5ad258243cb5696
Brayahn <brayahnmainah@gmail.com>
2024-11-28 15:35:44 +0300

    Reverse Trim Repeat and Trim Strings

---
+++ 24 Reverse, Repeat and Trim /script.js
@@ -0,0 +1,13 @@
+const reverseText = "test";
+
+const newText = reverseText.split("").reverse().join("");
+
+console.log(newText);
+
+// TRIM
+const myemail = " me@brianmaina.me     ";
+console.log(myemail.trim());
+
+//REPEAT
+const dogSays = "woof";
+console.log(dogSays.repeat(5)); // woofwoofwoofwoofwoof
---
+++ 25 String Test/script.js
@@ -0,0 +1,4 @@
+const guestList = "Our guests are: emma, jacob, isabella, ethan";
+
+const guestList_length = guestList.length;
+console.log(guestList_length);
