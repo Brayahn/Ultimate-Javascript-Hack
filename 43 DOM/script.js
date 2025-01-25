24af32cee3addf41e2a208a4c301bce4fe7d7a4d
Brayahn <brayahnmainah@gmail.com>
2025-01-25 11:01:41 +0300

    Intro to DOM

---
+++ 43 DOM/script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ DOM allows us to modify Web PAges HTML
+
+ Document Oject Model 
+
+ A standard to access and share documents/websites over the internet
+DOM defines how a Document/HTML_page is structured; how it can be modified(and its properties) and Add styles
+
+A tree of nested HHTML elements that are defiined whiile designingthe HTML pagee 
+ */
